ae50837dcc604fb4eef707714257f2bade1d3f3e
aeriel999 <ok4743@gmail.com>
2024-01-19 15:15:18 +0200

    .gitignore Fixed

---
+++ rivne.booking.org-server/rivne.booking.api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rivne.booking.Core.DTOs.Users;
+using rivne.booking.Core.Services;
+
+namespace rivne.booking.api.Controllers;
+
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : Controller
+{
+	private readonly UserService _userService;
+
+	public UserController(UserService userService)
+	{
+		_userService = userService;
+	}
+
+	[AllowAnonymous]
+	[HttpPost("login")]
+	public async Task<IActionResult> LoginUserAsync([FromBody] LoginUserDto model)
+	{
+		var result = await _userService.LoginUserAsync(model);
+		return Ok(result);
+	}
+}
---
+++ rivne.booking.org-server/rivne.booking.api/Program.cs
@@ -0,0 +1,118 @@
+using rivne.booking.Infrastructure;
+using rivne.booking.Core;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using rivne.booking.Infrastructure.Initializers;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Create connection sting
+string connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+// Create JWT Token Configuration
+var key = Encoding.UTF8.GetBytes(builder.Configuration["JwtConfig:Secret"]);
+
+var tokenValidationParemeters = new TokenValidationParameters
+{
+	ValidateIssuerSigningKey = true,
+	IssuerSigningKey = new SymmetricSecurityKey(key),
+	ValidateIssuer = true,
+	ValidateAudience = true,
+	ValidateLifetime = true,
+	ClockSkew = TimeSpan.Zero,
+	ValidIssuer = builder.Configuration["JwtConfig:Issuer"],
+	ValidAudience = builder.Configuration["JwtConfig:Audience"]
+};
+
+builder.Services.AddSingleton(tokenValidationParemeters);
+
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwt =>
+{
+	jwt.SaveToken = true;
+	jwt.TokenValidationParameters = tokenValidationParemeters;
+	jwt.RequireHttpsMetadata = false;
+});
+
+
+
+// Database context
+builder.Services.AddDbContext(connStr);
+
+// Add Core services
+builder.Services.AddCoreServices();
+
+// Add Infrastructure Service
+builder.Services.AddInfrastructureService();
+
+// Add Repositories
+builder.Services.AddRepositories();
+
+// Add Mapping
+builder.Services.AddMappings();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(option =>
+
+{
+	option.SwaggerDoc("v1", new OpenApiInfo { Title = "Dashboard API", Version = "v1" });
+
+	option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+
+	{
+		In = ParameterLocation.Header,
+
+		Description = "Please enter a valid token",
+
+		Name = "Authorization",
+
+		Type = SecuritySchemeType.Http,
+
+		BearerFormat = "JWT",
+
+		Scheme = "Bearer"
+
+	});
+
+	option.AddSecurityRequirement(new OpenApiSecurityRequirement
+		 {{ new OpenApiSecurityScheme
+			 {Reference = new OpenApiReference{Type=ReferenceType.SecurityScheme, Id="Bearer"}}, new string[]{}}});
+
+});
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseCors(options => options.SetIsOriginAllowed(origin => true).AllowAnyHeader().AllowCredentials().AllowAnyMethod());
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+//await ApiUsersInitializer.SeedPasswordsAndRoles(app);
+
+app.Run();
